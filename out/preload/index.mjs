57af77d58420b80812781b8ab5858f6b16948acb
Joao Vitor Rodrigues Morrudo <Joao.Morrudo@cyrela.com.br>
2026-02-27 16:36:04 -0300

    Removido pasta out finalmente

--- out/preload/index.mjs
+++
@@ -1,5 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-contextBridge.exposeInMainWorld("api", {
-  loadSettings: (defaults) => ipcRenderer.invoke("settings:load", defaults),
-  saveSettings: (data) => ipcRenderer.invoke("settings:save", data)
-});
